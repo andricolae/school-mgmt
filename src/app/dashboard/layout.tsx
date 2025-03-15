60a4e3aec2d6a98d5656f0e03a7c821f5df088a1
Andrei Nicolae <andreicalutiu@gmail.com>
2025-03-15 15:45:28 +0200

    menu and navbar design

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,10 +1,24 @@
+import Link from "next/link";
+import Image from "next/image"
+import Menu from "@/components/Menu";
+import Navbar from "@/components/Navbar";
+
 export default function DashboardLayout({
     children,
 }: Readonly<{
     children: React.ReactNode;
 }>) {
     return <div className="h-screen flex">
-        <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] bg-red-200">l</div>
-        <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-blue-200">r</div>
+        <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] p-4">
+            <Link href="/" className="flex items-center justify-center lg:justify-start gap-2"> 
+                <Image src="/logo.png" alt="logo" width={32} height={32} />
+                <span className="hidden lg:block">School MGMT</span>
+            </Link>
+            <Menu/>
+        </div>
+        <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-scroll">
+            <Navbar/>
+            {children}
+        </div>
     </div>
 }

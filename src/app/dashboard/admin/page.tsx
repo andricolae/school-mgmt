32feb4ddf527a8b7ba80bad0d9cb69df16672c2e
Andrei Nicolae <andreicalutiu@gmail.com>
2025-03-16 15:31:24 +0200

    admin dashboard design completed

--- src/app/dashboard/admin/page.tsx
+++ src/app/dashboard/admin/page.tsx
@@ -1,5 +1,7 @@
+import Announcements from "@/components/Announcements"
 import AttendanceChart from "@/components/AttendanceChart"
 import CountCharts from "@/components/CountChart"
+import EventCalendar from "@/components/EventCalendar"
 import FinanceChart from "@/components/FinanceChart"
 import UserCard from "@/components/UserCard"
 
@@ -25,8 +27,9 @@ const AdminPage = () => {
           <FinanceChart />
         </div>
       </div>
-      <div className="w-full lg:w-1/3">
-      
+      <div className="w-full lg:w-1/3 flex flex-col gap-8">
+        <EventCalendar />
+        <Announcements />
       </div>
     </div>
   )

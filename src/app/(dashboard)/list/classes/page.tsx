89fc511cc2ff3522af721bdabc04bc6092318c1e
Andrei Nicolae <andreicalutiu@gmail.com>
2025-05-03 17:19:05 +0300

    forms designed. final design implementation done

--- src/app/(dashboard)/list/classes/page.tsx
+++ src/app/(dashboard)/list/classes/page.tsx
@@ -1,3 +1,4 @@
+import FormModal from "@/components/FormModal"
 import Pagination from "@/components/Pagination"
 import Table from "@/components/Table"
 import TableSearch from "@/components/TableSearch"
@@ -49,15 +50,19 @@ const ClassListPage = () => {
             <td className="hidden md:table-cell">{item.supervisor}</td>
             <td>
                 <div className="flex items-center gap-2">
-                    <Link href={`/list/teachers/${item.id}`}>
+                    {/* <Link href={`/list/teachers/${item.id}`}>
                         <button className="w-7 h-7 flex items-center justify-center rounded-full bg-sky">
                             <Image src='/edit.png' alt="" width={16} height={16} />
                         </button>
-                    </Link>
+                    </Link> */}
                     {role === "admin" && (
-                        <button className="w-7 h-7 flex items-center justify-center rounded-full bg-orange">
-                            <Image src='/delete.png' alt="" width={16} height={16} />
-                        </button>
+                        // <button className="w-7 h-7 flex items-center justify-center rounded-full bg-orange">
+                        //     <Image src='/delete.png' alt="" width={16} height={16} />
+                        // </button>
+                        <>
+                            <FormModal table="class" type="delete" id={item.id} />
+                            <FormModal table="class" type="update" data={item}  />
+                        </>
                     )}
                 </div>
             </td>
@@ -78,9 +83,10 @@ const ClassListPage = () => {
                             <Image src="/sort.png" alt="" width={14} height={14} />
                         </button>
                         {role === "admin" && (
-                            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-yellow">
-                                <Image src="/plus.png" alt="" width={14} height={14} />
-                            </button>
+                            // <button className="w-8 h-8 flex items-center justify-center rounded-full bg-yellow">
+                            //     <Image src="/plus.png" alt="" width={14} height={14} />
+                            // </button>
+                            <FormModal table="class" type="create" />
                         )}
                     </div>
                 </div>
--- src/app/(dashboard)/list/exams/page.tsx
+++ src/app/(dashboard)/list/exams/page.tsx
@@ -1,3 +1,4 @@
+import FormModal from "@/components/FormModal"
 import Pagination from "@/components/Pagination"
 import Table from "@/components/Table"
 import TableSearch from "@/components/TableSearch"
@@ -48,15 +49,19 @@ const ExamListPage = () => {
             <td className="hidden md:table-cell">{item.date}</td>
             <td>
                 <div className="flex items-center gap-2">
-                    <Link href={`/list/teachers/${item.id}`}>
+                    {/* <Link href={`/list/teachers/${item.id}`}>
                         <button className="w-7 h-7 flex items-center justify-center rounded-full bg-sky">
                             <Image src='/edit.png' alt="" width={16} height={16} />
                         </button>
-                    </Link>
+                    </Link> */}
                     {role === "admin" && (
-                        <button className="w-7 h-7 flex items-center justify-center rounded-full bg-orange">
-                            <Image src='/delete.png' alt="" width={16} height={16} />
-                        </button>
+                        // <button className="w-7 h-7 flex items-center justify-center rounded-full bg-orange">
+                        //     <Image src='/delete.png' alt="" width={16} height={16} />
+                        // </button>
+                        <>
+                            <FormModal table="exam" type="delete" id={item.id} />
+                            <FormModal table="exam" type="update" data={item}  />
+                        </>
                     )}
                 </div>
             </td>
@@ -77,9 +82,10 @@ const ExamListPage = () => {
                             <Image src="/sort.png" alt="" width={14} height={14} />
                         </button>
                         {role === "admin" && (
-                            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-yellow">
-                                <Image src="/plus.png" alt="" width={14} height={14} />
-                            </button>
+                            // <button className="w-8 h-8 flex items-center justify-center rounded-full bg-yellow">
+                            //     <Image src="/plus.png" alt="" width={14} height={14} />
+                            // </button>
+                            <FormModal table="exam" type="create" />
                         )}
                     </div>
                 </div>

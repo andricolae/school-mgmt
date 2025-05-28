5106d0fa656d8b052e7b290fcda4dcc21b657220
Andrei Nicolae <andreicalutiu@gmail.com>
2025-05-28 20:32:20 +0300

    announcements fetching fixed

--- src/app/(dashboard)/list/announcements/page.tsx
+++ src/app/(dashboard)/list/announcements/page.tsx
@@ -11,131 +11,147 @@ import { auth } from "@clerk/nextjs/server";
 
 type AnnouncementList = Announcement & { class: Class };
 const AnnouncementListPage = async ({
-  searchParams,
+    searchParams,
 }: {
-  searchParams: { [key: string]: string | undefined };
+    searchParams: { [key: string]: string | undefined };
 }) => {
 
-  const { userId, sessionClaims } = auth();
-  const role = (sessionClaims?.metadata as { role?: string })?.role;
-  const currentUserId = userId;
+    const { userId, sessionClaims } = auth();
+    const role = (sessionClaims?.metadata as { role?: string })?.role;
+    const currentUserId = userId;
 
-  const columns = [
-    {
-      header: "Title",
-      accessor: "title",
-    },
-    {
-      header: "Class",
-      accessor: "class",
-    },
-    {
-      header: "Date",
-      accessor: "date",
-      className: "hidden md:table-cell",
-    },
-    ...(role === "admin"
-      ? [
+    const columns = [
         {
-          header: "Actions",
-          accessor: "action",
+            header: "Title",
+            accessor: "title",
         },
-      ]
-      : []),
-  ];
+        {
+            header: "Class",
+            accessor: "class",
+        },
+        {
+            header: "Date",
+            accessor: "date",
+            className: "hidden md:table-cell",
+        },
+        ...(role === "admin"
+            ? [
+                {
+                    header: "Actions",
+                    accessor: "action",
+                },
+            ]
+            : []),
+    ];
 
-  const renderRow = (item: AnnouncementList) => (
-    <tr
-      key={item.id}
-      className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-lamaPurpleLight"
-    >
-      <td className="flex items-center gap-4 p-4">{item.title}</td>
-      <td>{item.class?.name || "-"}</td>
-      <td className="hidden md:table-cell">
-        {new Intl.DateTimeFormat("en-US").format(item.date)}
-      </td>
-      <td>
-        <div className="flex items-center gap-2">
-          {role === "admin" && (
-            <>
-              <FormContainer table="announcement" type="update" data={item} />
-              <FormContainer table="announcement" type="delete" id={item.id} />
-            </>
-          )}
-        </div>
-      </td>
-    </tr>
-  );
-  const { page, ...queryParams } = searchParams;
+    const renderRow = (item: AnnouncementList) => (
+        <tr
+            key={item.id}
+            className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-lamaPurpleLight"
+        >
+            <td className="flex items-center gap-4 p-4">{item.title}</td>
+            <td>{item.class?.name || "-"}</td>
+            <td className="hidden md:table-cell">
+                {new Intl.DateTimeFormat("en-US").format(item.date)}
+            </td>
+            <td>
+                <div className="flex items-center gap-2">
+                    {role === "admin" && (
+                        <>
+                            <FormContainer table="announcement" type="update" data={item} />
+                            <FormContainer table="announcement" type="delete" id={item.id} />
+                        </>
+                    )}
+                </div>
+            </td>
+        </tr>
+    );
+    const { page, ...queryParams } = searchParams;
 
-  const p = page ? parseInt(page) : 1;
+    const p = page ? parseInt(page) : 1;
 
-  const query: Prisma.AnnouncementWhereInput = {};
+    const query: Prisma.AnnouncementWhereInput = {};
 
-  if (queryParams) {
-    for (const [key, value] of Object.entries(queryParams)) {
-      if (value !== undefined) {
-        switch (key) {
-          case "search":
-            query.title = { contains: value, mode: "insensitive" };
-            break;
-          default:
-            break;
+    if (queryParams) {
+        for (const [key, value] of Object.entries(queryParams)) {
+            if (value !== undefined) {
+                switch (key) {
+                    case "search":
+                        query.title = { contains: value, mode: "insensitive" };
+                        break;
+                    default:
+                        break;
+                }
+            }
         }
-      }
     }
-  }
 
-  const roleConditions = {
-    teacher: { lessons: { some: { teacherId: currentUserId! } } },
-    student: { students: { some: { id: currentUserId! } } },
-    parent: { students: { some: { parentId: currentUserId! } } },
-  };
+    const roleConditions = {
+        teacher: { lessons: { some: { teacherId: currentUserId! } } },
+        student: { students: { some: { id: currentUserId! } } },
+        parent: { students: { some: { parentId: currentUserId! } } },
+    };
 
-  query.OR = [
-    { classId: null },
-    {
-      class: roleConditions[role as keyof typeof roleConditions] || {},
-    },
-  ];
+    query.OR = [
+        { classId: null },
+        {
+            class: roleConditions[role as keyof typeof roleConditions] || {},
+        },
+    ];
 
-  const [data, count] = await prisma.$transaction([
-    prisma.announcement.findMany({
-      where: query,
-      include: {
-        class: true,
-      },
-      take: ITEM_PER_PAGE,
-      skip: ITEM_PER_PAGE * (p - 1),
+    const [data, count] = await prisma.$transaction([
+        prisma.announcement.findMany({
+        orderBy: { date: "desc" },
+        where: {
+            ...(role !== "admin" && {
+                OR: [
+                    { classId: null },
+                    { class: roleConditions[role as keyof typeof roleConditions] || {} }
+                ],
+            })
+        },
+        take: ITEM_PER_PAGE,
+        skip: ITEM_PER_PAGE * (p - 1),
     }),
-    prisma.announcement.count({ where: query }),
-  ]);
+        prisma.announcement.count({
+            where: {
+            ...(role !== "admin" && {
+                OR: [
+                    { classId: null },
+                    { class: roleConditions[role as keyof typeof roleConditions] || {} }
+                ],
+            })
+        },
+        }),
+    ]);
 
-  return (
-    <div className="bg-white p-4 rounded-md flex-1 m-4 mt-0">
-      <div className="flex items-center justify-between">
-        <h1 className="hidden md:block text-lg font-semibold">
-          All Announcements
-        </h1>
-        <div className="flex flex-col md:flex-row items-center gap-4 w-full md:w-auto">
-          <TableSearch />
-          <div className="flex items-center gap-4 self-end">
-            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
-              <Image src="/filter.png" alt="" width={14} height={14} />
-            </button>
-            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
-              <Image src="/sort.png" alt="" width={14} height={14} />
-            </button>
-            {role === "admin" && (
-              <FormContainer table="announcement" type="create" />
-            )}
-          </div>
+    console.log("\nitems per page", ITEM_PER_PAGE, "\npage", p, "\ncount", count);
+
+    return (
+        <div className="bg-white p-4 rounded-md flex-1 m-4 mt-0">
+            <div className="flex items-center justify-between">
+                <h1 className="hidden md:block text-lg font-semibold">
+                    All Announcements
+                </h1>
+                <div className="flex flex-col md:flex-row items-center gap-4 w-full md:w-auto">
+                    <TableSearch />
+                    <div className="flex items-center gap-4 self-end">
+                        <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
+                            <Image src="/filter.png" alt="" width={14} height={14} />
+                        </button>
+                        <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
+                            <Image src="/sort.png" alt="" width={14} height={14} />
+                        </button>
+                        {role === "admin" && (
+                            <FormContainer table="announcement" type="create" />
+                        )}
+                    </div>
+                </div>
+            </div>
+            <Table columns={columns} renderRow={renderRow} data={data} />
+            <Pagination page={p} count={count} />
         </div>
-      </div>
-      <Table columns={columns} renderRow={renderRow} data={data} />
-      <Pagination page={p} count={count} />
-    </div>
-  );
+    );
 };
 
 export default AnnouncementListPage;
